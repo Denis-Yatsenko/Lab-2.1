5fcb601c16dc003f424c64d1e510387ebf480768
Denys <Denys.Yatsenko.ITIS.2020@lpnu.ua>
2020-10-19 11:48:15 +0300

    first master commit

---
+++ Lab_2.1/Source.cpp
@@ -0,0 +1,27 @@
+// Lab_02.cpp
+// Яценко Денис
+// Лабораторна робота № 2.1
+// Лінійні програми.
+// Варіант 24
+
+#include <cmath>;
+#include <iostream>;
+using namespace std;
+
+int main()
+{
+	double a;     //вхідний параметр
+	double z1;    //результат обчислення 1-го виразу
+	//double z2;    //результат обчислення 2-го виразу
+
+	cout << "a = "; cin >> a;
+
+	z1 = (sin(a) + sin(5 * a) - sin(3 * a)) / (cos(a) - cos(3 * a) + cos(5 * a));
+	//z2 = tan(3 * a);
+	cout << endl;
+	cout << "z1 = " << z1 << endl;
+	//cout << "z2 = " << z2 << endl;
+
+	cin.get();
+	return 0;
+}
